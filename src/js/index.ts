d6ada212a7a2245658e8c261616fa94caf13b130
raihara3 <raihara3@gmail.com>
2020-11-14 17:10:32 +0900

    :construction: create app function

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,8 +1,27 @@
-class App {
-  create() {
-    console.log('hoge')
+class WebXR {
+  async supportedCheck() {
+    const supported = await navigator['xr'].isSessionSupported('immersive-ar')
+    if(!supported) {
+      console.error(`WebXR support: ${supported}`)
+    }
+    return supported
   }
 }
 
-const app = new App()
-app.create()
+const startWebXR = () => {
+  console.log('start')
+}
+
+const app = async() => {
+  const webXR = new WebXR()
+  const supported = await webXR.supportedCheck()
+
+  const startButton: HTMLButtonElement = <HTMLButtonElement> document.getElementById('start-button')
+  if(!supported) {
+    startButton.disabled = true
+    return
+  }
+  startButton.addEventListener('click', startWebXR)
+}
+
+app()
