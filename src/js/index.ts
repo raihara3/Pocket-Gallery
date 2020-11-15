4b15d836e93c46ff3d81f68d22bdeaaaa9d00dd6
raihara3 <raihara3@gmail.com>
2020-11-15 15:54:03 +0900

    :construction: render plane geometry

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,19 +1,33 @@
-class WebXR {
+import CreateScene, { renderer } from './CreateScene'
+
+class WebAR {
+  static mode = 'immersive-ar'
+
   async supportedCheck() {
-    const supported = await navigator['xr'].isSessionSupported('immersive-ar')
+    const supported = await navigator['xr'].isSessionSupported(WebAR.mode)
     if(!supported) {
       console.error(`WebXR support: ${supported}`)
     }
     return supported
   }
+
+  async requestSession() {
+    CreateScene()
+
+    const session = await navigator['xr'].requestSession(WebAR.mode)
+    renderer.xr.setReferenceSpaceType('local')
+    renderer.xr.setSession(session)
+  }
 }
 
+const webXR = new WebAR()
+
 const startWebXR = () => {
-  console.log('start')
+  console.debug('webXR start')
+  webXR.requestSession()
 }
 
 const app = async() => {
-  const webXR = new WebXR()
   const supported = await webXR.supportedCheck()
 
   const startButton: HTMLButtonElement = <HTMLButtonElement> document.getElementById('start-button')
