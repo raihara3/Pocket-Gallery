a100be331b4a17c92096bad936f093e0d1fe65e9
raihara3 <raihara3@gmail.com>
2020-11-24 22:41:01 +0900

    :memo: update ARObject class

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,16 +1,12 @@
 import * as THREE from 'three';
 import { ARButton } from './ARButton';
 
-let camera, scene, renderer;
-let controller;
-
-init();
-animate();
-
 class ARObject {
-  camera: THREE.Camera
+  camera: THREE.PerspectiveCamera
   scene: THREE.Scene
   renderer: THREE.WebGLRenderer
+  aspect: number
+  controller: THREE.Group
 
   constructor() {
     const fov = 70
@@ -18,9 +14,11 @@ class ARObject {
     const near = 0.01
     const far = 20
 
+    this.aspect = aspect
     this.camera = new THREE.PerspectiveCamera(fov, aspect, near, far)
     this.scene = new THREE.Scene()
     this.renderer = new THREE.WebGLRenderer({antialias: true, alpha: true})
+    this.controller = new THREE.Group()
   }
 
   init() {
@@ -28,49 +26,35 @@ class ARObject {
     this.renderer.setSize(window.innerWidth, window.innerHeight)
     this.renderer.xr.enabled = true
     document.body.appendChild(this.renderer.domElement)
+
+    let light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1)
+    light.position.set(0, 1, 0)
+    this.scene.add(light)
+
+    document.body.appendChild(ARButton.createButton(this.renderer,
+      {requiredFeatures: ['local', 'hit-test']}, this.scene)
+    )
+
+    window.addEventListener('resize', () => {
+      this.camera.aspect = this.aspect
+      this.camera.updateProjectionMatrix()
+      this.renderer.setSize(window.innerWidth, window.innerHeight)
+    }, false)
+
+    this.controller = this.renderer.xr.getController(0)
+    this.controller.addEventListener('selectend', () => {
+      this.controller.userData.isSelecting = true
+    })
+
+    this.animate()
+  }
+
+  animate() {
+    this.renderer.setAnimationLoop(() =>
+      this.renderer.render(this.scene, this.camera)
+    )
   }
 }
 
-function init() {
-
-  // 1. レンダラーのalphaオプションをtrueにする
-  renderer = new THREE.WebGLRenderer({antialias: true, alpha: true});
-
-  renderer.setPixelRatio(window.devicePixelRatio);
-  renderer.setSize(window.innerWidth, window.innerHeight);
-
-  // 2. レンダラーのXR機能を有効にする
-  renderer.xr.enabled = true;
-
-  document.body.appendChild(renderer.domElement);
-
-  scene = new THREE.Scene();
-  let light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
-  light.position.set(0, 1, 0);
-  scene.add(light);
-
-  // 3. AR表示を有効にするためのボタンを画面に追加する
-  document.body.appendChild(ARButton.createButton(renderer, {requiredFeatures: ['local', 'hit-test']}, scene));
-
-  camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 20);
-  window.addEventListener('resize', () => {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-  }, false);
-
-  // 4. XRセッションにアクセスするためコントローラーを取得する
-  controller = renderer.xr.getController(0);
-  controller.addEventListener('selectend', () => {
-    controller.userData.isSelecting = true;
-  });
-}
-
-function animate() {
-  renderer.setAnimationLoop(render);
-}
-
-function render() {
-  // handleController(controller);
-  renderer.render(scene, camera);
-}
+const arObject = new ARObject()
+arObject.init()
